ed1ee2ce0052b31872f6ff46b4b791e9d648f80a
chendq@e-gets.com <chendq@e-gets.com>
2019-05-24 10:13:41 +0700

    update条形码生成

--- application/modules/Api/controllers/Pay.php
+++ application/modules/Api/controllers/Pay.php
@@ -347,4 +347,43 @@ class PayController extends \Base\ApiController
     }
 
 
+    public function barcodeAction(){
+        $orderSn = $this->getParam('orderSn','','string');
+        $order = \M\Mapper\MytTradeOrder::getInstance()->findByOut_trade_no($orderSn);
+        if(!$order instanceof \M\MytTradeOrder){
+            throw new Exception('订单不存在');
+        }
+        // 加载字体大小
+        $font = new \BarcodeBakery\Common\BCGFontFile(PUBLIC_PATH.'/../data/fonts/AntykwaBold.ttf', 18);
+        //颜色条形码
+        $color_black = new \BarcodeBakery\Common\BCGColor(0, 0, 0);
+        $color_white = new \BarcodeBakery\Common\BCGColor(255, 255, 255);
+
+        $drawException = null;
+        try {
+            $code = new \BarcodeBakery\Barcode\BCGcode128();
+            $code->setScale(2);
+            $code->setThickness(30); // 条形码的厚度
+            $code->setForegroundColor($color_black); // 条形码颜色
+            $code->setBackgroundColor($color_white); // 空白间隙颜色
+            $code->setFont($font); //
+            $code->parse($order->getOut_trade_no()); // 条形码需要的数据内容
+        } catch(Exception $exception) {
+            $drawException = $exception;
+        }
+
+        //根据以上条件绘制条形码
+        $drawing = new \BarcodeBakery\Common\BCGDrawing('', $color_white);
+        if($drawException) {
+            $drawing->drawException($drawException);
+        } else {
+            $drawing->setBarcode($code);
+            $drawing->draw();
+        }
+        // 生成PNG格式的图片
+        header('Content-Type: image/png');
+        $drawing->finish(\BarcodeBakery\Common\BCGDrawing::IMG_FORMAT_PNG);
+    }
+
+
 }
